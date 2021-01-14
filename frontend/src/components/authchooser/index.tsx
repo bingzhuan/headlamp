d47a00f410a67480ad267b3b5f4634523f995f78
René Dudfield <rene@kinvolk.io>
2021-01-14 13:09:05 +0100

    frontend: add src/components/authchooser stories

--- frontend/src/components/authchooser/index.tsx
+++ frontend/src/components/authchooser/index.tsx
@@ -23,8 +23,8 @@ const ColorButton = withStyles((theme) => ({
     padding: '0.5rem 2rem',
     '&:hover': {
       opacity: '0.8',
-      backgroundColor: theme.palette.sidebarBg
-    }
+      backgroundColor: theme.palette.sidebarBg,
+    },
   },
 }))(Button);
 
@@ -32,15 +32,17 @@ interface ReactRouterLocationStateIface {
   from?: Location;
 }
 
-function AuthChooser(){
+function AuthChooser() {
   const history = useHistory();
   const location = useLocation();
   const clusters = useClustersConf();
   const dispatch = useDispatch();
-  const isDevMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+  const isDevMode =
+    !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
   const [testingAuth, setTestingAuth] = React.useState(false);
   const [error, setError] = React.useState<Error | null>(null);
-  const {from = { pathname: createRouteURL('cluster') }} = (location.state || {}) as ReactRouterLocationStateIface;
+  const { from = { pathname: createRouteURL('cluster') } } = (location.state ||
+    {}) as ReactRouterLocationStateIface;
   const clusterName = getCluster() as string;
 
   let clusterAuthType = '';
@@ -48,168 +50,220 @@ function AuthChooser(){
     clusterAuthType = clusters[clusterName].auth_type;
   }
 
-  function handleTokenAuth() {
-    history.push({
-      pathname: generatePath(getRoutePath(getRoute('token')), {cluster: clusterName as string}),
-    });
-  }
-
-  function handleOidcAuth() {
-    history.replace({
-      pathname: generatePath(getClusterPrefixedPath(), {cluster: clusterName as string}),
-    });
-  }
-
-  function handleBackButtonPress(){
-    history.goBack();
-  }
-
   const numClusters = Object.keys(clusters || {}).length;
 
-  React.useEffect(() => {
-    const clusterName = getCluster();
-    if (!clusterName || !clusters || error || numClusters === 0) {
-      return;
-    }
+  React.useEffect(
+    () => {
+      const clusterName = getCluster();
+      if (!clusterName || !clusters || error || numClusters === 0) {
+        return;
+      }
 
-    const cluster = clusters[clusterName];
-    if (!cluster) {
-      return;
-    }
+      const cluster = clusters[clusterName];
+      if (!cluster) {
+        return;
+      }
 
-    let cancelled = false;
+      let cancelled = false;
 
-    // If we haven't yet figured whether we need to use a token for the current
-    // cluster, then we check here.
-    if (cluster.useToken === undefined) {
-      let useToken = true;
+      // If we haven't yet figured whether we need to use a token for the current
+      // cluster, then we check here.
+      if (cluster.useToken === undefined) {
+        let useToken = true;
 
-      setTestingAuth(true);
+        setTestingAuth(true);
 
-      let errorObj: Error | null = null;
-      setError(errorObj);
+        let errorObj: Error | null = null;
+        setError(errorObj);
 
-      testAuth()
-        .then(() => {
-          console.debug('Not requiring token as testing auth succeeded');
-          useToken = false;
-        })
-        .catch((err) => {
-          if (!cancelled) {
-            console.debug('Requiring token as testing auth failed:', err);
+        testAuth()
+          .then(() => {
+            console.debug('Not requiring token as testing auth succeeded');
+            useToken = false;
+          })
+          .catch((err) => {
+            if (!cancelled) {
+              console.debug('Requiring token as testing auth failed:', err);
 
-            // Ideally we'd only not assign the error if it was 401 or 403 (so we let the logic
-            // proceed to request a token), but let's first check whether this is all we get
-            // from clusters that require a token.
-            if ([408, 504, 502].includes(err.status)) {
-              errorObj = err;
+              // Ideally we'd only not assign the error if it was 401 or 403 (so we let the logic
+              // proceed to request a token), but let's first check whether this is all we get
+              // from clusters that require a token.
+              if ([408, 504, 502].includes(err.status)) {
+                errorObj = err;
+              }
+
+              setTestingAuth(false);
             }
+          })
+          .finally(() => {
+            if (!cancelled) {
+              cancelled = true;
+              setTestingAuth(false);
 
-            setTestingAuth(false);
-          }
-        })
-        .finally(() => {
-          if (!cancelled) {
-            cancelled = true;
-            setTestingAuth(false);
+              if (!!errorObj) {
+                setError(errorObj);
+                return;
+              }
 
-            if (!!errorObj) {
-              setError(errorObj);
-              return;
+              cluster.useToken = useToken;
+              dispatch(setConfig({ clusters: { ...clusters } }));
+              // If we don't require a token, then we just move to the attempted URL or root.
+              if (!useToken) {
+                history.replace(from);
+              }
+
+              // If we reach this point, then we know whether or not we need a token. If we don't,
+              // just redirect.
+              if (cluster.useToken === false) {
+                history.replace(from);
+              } else if (!clusterAuthType) {
+                // we know that it requires token and also doesn't have oidc configured
+                // so let's redirect to token page
+                history.replace({
+                  pathname: generatePath(getClusterPrefixedPath('token'), {
+                    cluster: clusterName as string,
+                  }),
+                });
+              }
             }
+          });
+      } else if (cluster.useToken) {
+        history.replace({
+          pathname: generatePath(getClusterPrefixedPath('token'), {
+            cluster: clusterName as string,
+          }),
+        });
+      }
 
-            cluster.useToken = useToken;
-            dispatch(setConfig({clusters: {...clusters}}));
-            // If we don't require a token, then we just move to the attempted URL or root.
-            if (!useToken) {
-              history.replace(from);
-            }
-
-            // If we reach this point, then we know whether or not we need a token. If we don't,
-            // just redirect.
-            if (cluster.useToken === false) {
-              history.replace(from);
-            } else if (!clusterAuthType){
-              // we know that it requires token and also doesn't have oidc configured
-              // so let's redirect to token page
-              history.replace({
-                pathname: generatePath(getClusterPrefixedPath('token'), {cluster: clusterName as string}),
-              });
-            }
-          }
-        }
-        );
-    } else if (cluster.useToken) {
-      history.replace({
-        pathname: generatePath(getClusterPrefixedPath('token'), {cluster: clusterName as string}),
-      });
-    }
-
-    return function cleanup () {
-      cancelled = true;
-    };
-  },
-  // eslint-disable-next-line
-  [clusters, error]);
+      return function cleanup() {
+        cancelled = true;
+      };
+    },
+    // eslint-disable-next-line
+    [clusters, error]
+  );
 
   return (
-    <ClusterDialog
-      useCover
-      disableEscapeKeyDown
-      disableBackdropClick
-    >
-      {testingAuth ?
+    <PureAuthChooser
+      testingTitle={
+        numClusters > 1
+          ? `Getting auth info: ${clusterName}`
+          : 'Getting auth info'
+      }
+      testingAuth={testingAuth}
+      title={
+        numClusters > 1 ? `Authentication: ${clusterName}` : 'Authentication'
+      }
+      haveClusters={!!clusters && Object.keys(clusters).length > 1}
+      error={error}
+      oauthUrl={`${
+        isDevMode || isElectron() ? 'http://localhost:4466/' : '/'
+      }oidc?dt=${Date()}&cluster=${getCluster()}`}
+      clusterAuthType={clusterAuthType}
+      handleTryAgain={() => setError(null)}
+      handleOidcAuth={() => {
+        history.replace({
+          pathname: generatePath(getClusterPrefixedPath(), {
+            cluster: clusterName as string,
+          }),
+        });
+      }}
+      handleBackButtonPress={() => {
+        history.goBack();
+      }}
+      handleTokenAuth={() => {
+        history.push({
+          pathname: generatePath(getRoutePath(getRoute('token')), {
+            cluster: clusterName as string,
+          }),
+        });
+      }}
+    />
+  );
+}
+
+export interface PureAuthChooserProps {
+  title: string;
+  testingTitle: string;
+  testingAuth: boolean;
+  error: Error | null;
+  oauthUrl: string;
+  clusterAuthType: string;
+  haveClusters: boolean;
+  handleOidcAuth: () => void;
+  handleTokenAuth: () => void;
+  handleTryAgain: () => void;
+  handleBackButtonPress: () => void;
+}
+
+export function PureAuthChooser({
+  title,
+  testingTitle,
+  testingAuth,
+  error,
+  oauthUrl,
+  clusterAuthType,
+  haveClusters,
+  handleOidcAuth,
+  handleTokenAuth,
+  handleTryAgain,
+  handleBackButtonPress,
+}: PureAuthChooserProps) {
+  return (
+    <ClusterDialog useCover disableEscapeKeyDown disableBackdropClick>
+      {testingAuth ? (
         <Box textAlign="center">
-          <DialogTitle>
-            { numClusters > 1 ? `Getting auth info: ${clusterName}` : 'Getting auth info' }
-          </DialogTitle>
-          <Loader/>
+          <DialogTitle>{testingTitle}</DialogTitle>
+          <Loader />
         </Box>
-        :
+      ) : (
         <Box display="flex" flexDirection="column" alignItems="center">
-          <DialogTitle>
-            { numClusters > 1 ? `Authentication: ${clusterName}` : 'Authentication' }
-          </DialogTitle>
-          { !error ?
+          <DialogTitle>{title}</DialogTitle>
+          {!error ? (
             <Box>
-              {
-                clusterAuthType === 'oidc' ?
-                  <Box m={2}>
-                    <OauthPopup onCode={handleOidcAuth}
-                      url={`${isDevMode || isElectron() ?
-                        'http://localhost:4466/' : '/'}oidc?dt=${Date()}&cluster=${getCluster()}`
-                      }
-                      title="Headlamp Cluster Authentication"
-                    >
-                      <ColorButton>Sign In</ColorButton>
-                    </OauthPopup>
-                  </Box>
-                  : null
-              }
+              {clusterAuthType === 'oidc' ? (
+                <Box m={2}>
+                  <OauthPopup
+                    onCode={handleOidcAuth}
+                    url={oauthUrl}
+                    title="Headlamp Cluster Authentication"
+                  >
+                    <ColorButton>Sign In</ColorButton>
+                  </OauthPopup>
+                </Box>
+              ) : null}
               <Box m={2}>
                 <ColorButton onClick={handleTokenAuth}>Use A Token</ColorButton>
               </Box>
             </Box>
-            :
+          ) : (
             <Box alignItems="center" textAlign="center">
               <Box m={2}>
-                <Empty>Failed to get authentication information: {error!.message}</Empty>
+                <Empty>
+                  Failed to get authentication information: {error!.message}
+                </Empty>
               </Box>
-              <ColorButton onClick={() => setError(null)}>Try Again</ColorButton>
+              <ColorButton onClick={handleTryAgain}>Try Again</ColorButton>
             </Box>
-          }
+          )}
         </Box>
-      }
-      {!!clusters && Object.keys(clusters).length > 1 &&
+      )}
+      {haveClusters && (
         <Box display="flex" flexDirection="column" alignItems="center">
-          <Box m={2} display="flex" alignItems="center" style={{cursor: 'pointer'}} onClick={handleBackButtonPress}>
+          <Box
+            m={2}
+            display="flex"
+            alignItems="center"
+            style={{ cursor: 'pointer' }}
+            onClick={handleBackButtonPress}
+          >
             <Box pt={0.5}>
-              <InlineIcon icon={chevronLeft} height={20} width={20}/>
+              <InlineIcon icon={chevronLeft} height={20} width={20} />
             </Box>
             <Box fontSize={14}>BACK</Box>
           </Box>
         </Box>
-      }
+      )}
     </ClusterDialog>
   );
 }
